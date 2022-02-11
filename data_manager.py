2739e1f90134cf7532c2e03c93917f52b044ae2b
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-02-11 13:35:35 +0100

    Add DataManager class

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+SHEETY_ENDPOINT = os.getenv("SHEETY_ENDPOINT")
+TOKEN = os.getenv("TOKEN")
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.bearer_headers = {"Authorization": TOKEN}
+
+    def read_sheety(self):
+        response = requests.get(url=SHEETY_ENDPOINT, headers=self.bearer_headers)
+        response_json = response.json()
+        return response_json
+
+    def write_sheety(self, json_data):
+        response = requests.post(url=SHEETY_ENDPOINT, json=json_data, headers=self.bearer_headers)
+        return response
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # Day 39 capstone project - Flight Deal Finder.
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve
 # the program requirements.
+
+import data_manager
+
+sheety_manager = data_manager.DataManager()
+
