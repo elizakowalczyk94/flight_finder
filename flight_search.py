130d5d10b57169625a906bd85a8cd441f5db2f05
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-02-20 09:15:17 +0100

    Add kiwi link

--- flight_search.py
+++ flight_search.py
@@ -5,7 +5,7 @@ import flight_data
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("venv/.env")
 
 KIWI_ENDPOINT = os.getenv("KIWI_ENDPOINT")
 KIWI_KEY = os.getenv("KIWI_KEY")
@@ -14,36 +14,19 @@ KIWI_KEY = os.getenv("KIWI_KEY")
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
 
-    def __init__(self, departure_airport, arrival_airports):
-        self.sheety_manager = data_manager.DataManager()
+    def __init__(self, departure_iata, arrival_iata):
         self.kiwi_iata_endpoint = KIWI_ENDPOINT + "/locations/query"
         self.kiwi_key = {"apikey": KIWI_KEY}
-
-        self.departure_airport = departure_airport
-        self.arrival_airports = arrival_airports
         self.kiwi_flight_data = KIWI_ENDPOINT + "/v2/search"
+        self.departure_iata = departure_iata
+        self.arrival_iata = arrival_iata
         self.tomorrow_date = datetime.datetime.today() + datetime.timedelta(days=1)
         self.half_year = datetime.datetime.today() + datetime.timedelta(days=6 * 30)
 
-    def write_iata(self, city_name):
-        city_name = city_name.title()
-        city_list = self.sheety_manager.read_sheety()["prices"]
-        for city_dict in city_list:
-            if city_dict["city"] == city_name:
-                obj_id = city_dict["id"]
-                kiwi_json = requests.get(url=self.kiwi_iata_endpoint,
-                                         params={"term": city_name},
-                                         headers=self.kiwi_key).json()
-                iata = kiwi_json["locations"][0]["code"]
-                iata_json = {"price": {"iataCode": iata}}
-                self.sheety_manager.modify_sheety(obj_id=obj_id, json_data=iata_json)
-            else:
-                return False
-
     def search_direct_flights(self):
         search_flight_params = {
-            "fly_from": self.departure_airport,
-            "fly_to": self.arrival_airports,
+            "fly_from": self.departure_iata,
+            "fly_to": self.arrival_iata,
             "date_from": self.tomorrow_date.strftime("%d/%m/%Y"),
             "date_to": self.half_year.strftime("%d/%m/%Y"),
             "nights_in_dst_from": 7,
@@ -54,21 +37,20 @@ class FlightSearch:
             "curr": "PLN"}
 
         flight_kiwi = requests.get(url=self.kiwi_flight_data, params=search_flight_params, headers=self.kiwi_key)
-
         try:
-            print(flight_kiwi.json()["data"][0])
             flight_kiwi = flight_kiwi.json()["data"][0]
         except IndexError:
-            print("No flight found for the destination.")
-            return None
+            print(f"No flight found for {self.arrival_iata}.")
+            return False
 
         flight_data_kiwi = flight_data.FlightData(
             price=flight_kiwi["price"],
             departure_city=flight_kiwi["cityFrom"],
-            departure_airport=flight_kiwi["cityCodeFrom"],
+            departure_airport=flight_kiwi["flyFrom"],
             arrival_city=flight_kiwi["cityTo"],
-            arrival_airport=flight_kiwi["cityCodeTo"],
+            arrival_airport=flight_kiwi["flyTo"],
             departure_date=flight_kiwi["local_departure"].split("T")[0],
-            arrival_date=flight_kiwi["local_arrival"].split("T")[0])
+            airline=flight_kiwi["airlines"][0],
+            kiwi_link=flight_kiwi["deep_link"])
 
         return flight_data_kiwi
