e0672977c4445fc28165fd2c176861f975dc26ef
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-02-20 15:44:43 +0100

    Create message

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Day 39 capstone project - Flight Deal Finder.
-# This file will need to use the DataManager, FlightSearch, FlightData, NotificationManager classes to achieve
-# the program requirements.
 
 import data_manager
 import flight_search
 import notification_manager
 
-DEPARTURE_CITY = "Warsaw"
+DEPARTURE_CITY = "Katowice"
+RECEIVER_EMAIL = ""
 
-csv_manager = data_manager.DataManager(cities_list=["Paris", "New york", "CRACOW"], prices_list=[400, 3000, 150])
+csv_manager = data_manager.DataManager(cities_list=["Barcelona"], prices_list=[400])
 csv_data = csv_manager.city_file
 DEPARTURE_IATA = csv_manager.get_iata(DEPARTURE_CITY)
 
@@ -16,5 +15,9 @@ for destination_iata in csv_data.iata:
     flight_object = flight_search.FlightSearch(departure_iata=DEPARTURE_IATA, arrival_iata=destination_iata)
     flight_found = flight_object.search_direct_flights()
     if flight_found:
-        print(DEPARTURE_CITY, "-->", flight_found.arrival_city, flight_found.departure_date,
-              "with", flight_found.airline, flight_found.kiwi_link)
+        if flight_found.price <= 400:
+            message = f"{DEPARTURE_CITY} {flight_found.departure_date} --> {flight_found.arrival_city} " \
+                      f"{flight_found.return_date}. Total price {flight_found.price}PLN. " \
+                      f"\n\nCheck the details {flight_found.kiwi_link}"
+            email_sender = notification_manager.NotificationManager(RECEIVER_EMAIL, message)
+            email_sender.send_email()
