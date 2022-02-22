42db4b1961a8d995a32868d2dc1d18bc7a3bc814
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-02-22 18:28:23 +0100

    Ad stop_overs and via_city

--- flight_data.py
+++ flight_data.py
@@ -1,7 +1,8 @@
 class FlightData:
 
     def __init__(self, price, departure_city, departure_airport, arrival_city,
-                 arrival_airport, departure_date, airline, kiwi_link, return_date):
+                 arrival_airport, departure_date, airline, kiwi_link, return_date,
+                 stop_overs=0, via_city=""):
         self.price = price
         self.departure_city = departure_city
         self.departure_airport = departure_airport
@@ -11,3 +12,5 @@ class FlightData:
         self.return_date = return_date
         self.airline = airline
         self.kiwi_link = kiwi_link
+        self.stop_overs = stop_overs
+        self.via_city = via_city
