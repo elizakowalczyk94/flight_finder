9ae64615085fa39232af2991f47d749b1070a8b5
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-02-22 18:45:50 +0100

    Add send_email_to_all_users()

--- notification_manager.py
+++ notification_manager.py
@@ -1,31 +1,39 @@
 import smtplib
 import ssl
 import os
+
+import pandas
 from dotenv import load_dotenv
 
 load_dotenv("venv/.env")
 
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+USER_DATA_CSV = "users_data.csv"
 
 
 class NotificationManager:
 
-    def __init__(self, receiver_email, message):
+    def __init__(self):
         self.port = 465
         self.smtp_server = "smtp.gmail.com"
         self.sender_email = SENDER_EMAIL
-        self.receiver_email = receiver_email
         self.password = EMAIL_PASSWORD
-        self.message = message
         self.email_message = ""
-        self.create_message()
+        self.receivers_emails = []
+        self.read_users_data()
 
-    def create_message(self):
-        self.email_message = f"Subject: Flight alert \n\n {self.message}"
-
-    def send_email(self):
+    def send_email(self, message, receiver_email):
+        self.email_message = f"Subject: Flight alert \n\n {message}"
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(self.smtp_server, self.port, context=context) as server:
             server.login(self.sender_email, self.password)
-            server.sendmail(self.sender_email, self.receiver_email, self.email_message)
+            server.sendmail(self.sender_email, receiver_email, self.email_message)
+
+    def read_users_data(self):
+        users_data = pandas.read_csv(USER_DATA_CSV)
+        self.receivers_emails = users_data.user_email.tolist()
+
+    def send_email_to_all_users(self, message):
+        for user_email in self.receivers_emails:
+            self.send_email(message, user_email)
